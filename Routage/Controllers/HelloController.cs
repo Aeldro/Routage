06a003aaf046e9ddbd9ae453d3430bca18e58d5a
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-07-17 10:51:41 +0200

    Ajoutez des fichiers projet.

---
+++ Routage/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routage.Controllers
+{
+    public class HelloController : Controller
+    {
+
+        public string SayHello() => "Hello !";
+        public string YellHello() => "HELLOOOOO !";
+
+        [Route("/Say")]
+        public string SayHelloAttributes() => "Hello !";
+
+        [Route("/Yell")]
+        public string YellHelloAttributes() => "HELLOOOOO !";
+    }
+}
